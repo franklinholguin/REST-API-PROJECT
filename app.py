603c24a709611b9dd9a227fff556c4cbc7c2af97
franklinholguin <franklin.holguin@gmail.com>
2023-04-28 23:09:34 -0500

    Adding Flask-Smorest for Improve  Efficientcy

--- app.py
+++ app.py
@@ -8,18 +8,12 @@ from db import stores, items
 app = Flask(__name__)
 
 
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404, message="Item not found.")
-
-
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
-
+    # Here not only we need to validate data exists,
+    # But also what type of data. Price should be a float,
+    # for example.
     if (
         "price" not in item_data
         or "store_id" not in item_data
@@ -29,6 +23,7 @@ def create_item():
             400,
             message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
         )
+
     for item in items.values():
         if (
             item_data["name"] == item["name"]
@@ -41,6 +36,76 @@ def create_item():
     items[item_id] = item
 
     return item
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    # Here not only we need to validate data exists,
+    # But also what type of data. Price should be a float,
+    # for example.
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    # There's  more validation to do here!
+    # Like making sure price is a number, and also both items are optional
+    # Difficult to do with an if statement...
+    if "price" not in item_data or "name" not in item_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
+        )
+    try:
+        item = items[item_id]
+
+        # https://blog.teclado.com/python-dictionary-merge-update-operators/
+        item |= item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
 
 
 @app.get("/item")
@@ -75,6 +140,15 @@ def create_store():
     stores[store_id] = store
 
     return store
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
 
 
 @app.get("/store")
